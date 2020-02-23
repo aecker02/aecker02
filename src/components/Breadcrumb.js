efaff9bd2465a646fcc9f2881468dd681aaaf742
Craig Jones <craig@residently.com>
2020-02-23 06:07:08 +0000

    style breadcrumbs

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import { toTitleCase } from '../utils/helpers'
+import React from "react";
+import { Link } from "gatsby";
+import { toTitleCase } from "../utils/helpers";
 
-const Breadcrumb = ({currentPage, parentPage}) => {
+const Breadcrumb = ({ currentPage, isMaterial, parentPage }) => {
   return (
     <nav className="breadcrumb">
-      <ol>
-        <li><a href="/">Home</a></li>
-        {parentPage && <li><a href={`/${parentPage}`}>{toTitleCase(parentPage)}</a></li> }
-        <li>{toTitleCase(currentPage)}</li>
-      </ol>
+      <ul className="breadcrumb__inner">
+        <li>
+          <Link className="breadcrumb__item" to="/">
+            Home
+          </Link>
+        </li>
+        {isMaterial && (
+          <li>
+            <Link className="breadcrumb__item" to={`/materials`}>
+              Materials
+            </Link>
+          </li>
+        )}
+        {parentPage && (
+          <li>
+            <Link className="breadcrumb__item" to={`/${parentPage}`}>
+              {toTitleCase(parentPage)}
+            </Link>
+          </li>
+        )}
+        <li className="breadcrumb__item breadcrumb__item--current">
+          {toTitleCase(currentPage)}
+        </li>
+      </ul>
     </nav>
   );
 };
--- src/templates/categoryPage.js
+++ src/templates/categoryPage.js
@@ -1,28 +1,36 @@
 import React from "react";
-import Img from 'gatsby-image'
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
-import Breadcrumb from '../components/Breadcrumb'
-import { toLowerKebabCase } from '../utils/helpers'
+import Img from "gatsby-image";
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
+import Breadcrumb from "../components/Breadcrumb";
+import { toLowerKebabCase } from "../utils/helpers";
 
-const CategoryPage = ({pageContext}) => {
+const CategoryPage = ({ pageContext }) => {
   const { title, material } = pageContext;
 
   return (
     <Layout>
-    <SEO title={title} />
-    <Breadcrumb currentPage={toLowerKebabCase(title)} />
+      <SEO title={title} />
+      <Breadcrumb currentPage={toLowerKebabCase(title)} isMaterial={true} />
       <h1>{title}</h1>
       {material.map(mat => {
         return (
-          <a key={mat.name} href={`/${toLowerKebabCase(mat.category)}/${toLowerKebabCase(mat.name)}`}>
+          <a
+            key={mat.name}
+            href={`/${toLowerKebabCase(mat.category)}/${toLowerKebabCase(
+              mat.name
+            )}`}
+          >
             <h2>{mat.name}</h2>
-            <Img fluid={mat.imageSharp.childImageSharp.fluid} style={{width: 300}}/>
+            <Img
+              fluid={mat.imageSharp.childImageSharp.fluid}
+              style={{ width: 300 }}
+            />
             <h3>{mat.origin_country}</h3>
           </a>
         );
       })}
-  </Layout>
+    </Layout>
   );
 };
 
