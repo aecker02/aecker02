e57331138bc4b0e073714f84a52d475c2dea17fc
Craig Jones <craig@residently.com>
2020-02-24 08:08:30 +0000

    we go again

--- src/components/header.js
+++ src/components/header.js
@@ -35,19 +35,6 @@ const Header = ({ breakpoints, currentBreakpoint }) => {
 };
 
 const RenderNav = () => {
-  const data = useStaticQuery(graphql`
-    query NavMaterialItems {
-      prismic {
-        allMaterialss {
-          edges {
-            node {
-              title
-            }
-          }
-        }
-      }
-    }
-  `);
   return (
     <nav className="header__nav">
       <ul>
