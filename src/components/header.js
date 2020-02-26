f9355526a1c3607814d9cfd979c85a7513ab2e6e
Andy Coupe <andrewmcoupe@gmail.com>
2020-02-26 06:30:08 +0000

    start dynamic nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import { Link, graphql, useStaticQuery } from "gatsby";
 import React, { useState } from "react";
 import { withBreakpoints } from "react-breakpoints";
+import { usePrismicNavItems } from "./header.hooks";
+import cx from "classnames";
 
 const Header = ({ breakpoints, currentBreakpoint }) => {
   const [navOpen, setNavOpen] = useState(false);
@@ -32,6 +34,27 @@ const Header = ({ breakpoints, currentBreakpoint }) => {
 };
 
 const RenderNav = () => {
+  const [activeNavItem, setActiveNavItem] = React.useState("");
+  const navData = usePrismicNavItems();
+
+  const MaterialNavItems = Object.keys(navData).map((material, index) => {
+    return (
+      <li key={index} className="header__nav--item">
+        {material}
+        <ul className="header__nav--sub-items">
+          {navData[material].map((child, index) => (
+            <li key={index}>{child}</li>
+          ))}
+        </ul>
+      </li>
+    );
+  });
+
+  const handleNavItemClick = (evt, itemName) => {
+    evt.preventDefault();
+    setActiveNavItem(itemName);
+  };
+
   return (
     <nav className="header__nav">
       <ul>
@@ -39,7 +62,13 @@ const RenderNav = () => {
           <Link to="/">Home</Link>
         </li>
         <li className="header__nav--item">
-          <Link to="/materials">Materials</Link>
+          <Link
+            to="/materials"
+            onClick={evt => handleNavItemClick(evt, "materials")}
+          >
+            Materials
+          </Link>
+          {activeNavItem === "materials" && <ul>{MaterialNavItems}</ul>}
         </li>
         <li className="header__nav--item">
           <Link to="/services">Services</Link>
